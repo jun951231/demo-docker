c85848428cf087784d5797e75cbeed77158889dc
jun951231 <jun951231@gmail.com>
2021-08-12 09:36:49 +0900

    Aug-11

--- src/main/java/com/example/jun/demo/calculator/entity/User.java
+++ src/main/java/com/example/jun/demo/calculator/entity/User.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collation = "users")
+@Document(collection = "users")
 public class User {
     @Id
     private String userid;
